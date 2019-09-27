7780b037170a07a68225a7c2298318b63b7f015a
Alex  Olteanu <alexolteanu@Alexs-MacBook-Pro-2.local>
2019-09-27 09:05:41 -0400

    added 08_firstApp.py

---
+++ 08_firstApp.py
@@ -0,0 +1,23 @@
+rom flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    print(__name__)
+    return "No hablo queso!"
+
+@app.route("apple")
+def eat():
+    return "yum!"
+
+@app.route("bed")
+def sleep():
+    return "catch some z's"
+
+@app.route("toothbrush")
+def brush():
+    return "pearly whites!"
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
