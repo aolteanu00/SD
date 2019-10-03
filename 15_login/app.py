b4cae77b660221937c80a75c00dcc44b399aeb55
Alex  Olteanu <alexolteanu@Alexs-MacBook-Pro-2.local>
2019-10-03 17:00:25 -0400

    updated 15_login

--- 15_login/app.py
+++ 15_login/app.py
@@ -1,14 +1,38 @@
 from flask import Flask, render_template, request
+from flask import redirect
+from flask import session
+from flask import url_for
+import os
 app = Flask(__name__)
+app.secret_key = os.urandom(32)
+
 
 
 @app.route("/")
 def login():
+    global entered
+    entered = False
     #print (request.args["username"])
-    return render_template('home.html')
+    global userw
+    userw = ""
+    global passw
+    passw = ""
+    username = "alex"
+    password = "pass"
+
+    return render_template('home.html', error = entered, passw = passw, userw = userw)
+
+@app.route("/login")
+def after():
+    username = "alex"
+    password = "pass"
+    entered = True
+
+    return render_template('home.html', error = entered, passw = passw, userw = userw)
 
 @app.route("/welcome")
 def stuff():
+    entered = False
     username = "alex"
     password = "pass"
     print(request.form)
@@ -16,10 +40,15 @@ def stuff():
     print(request.method)
     print(app)
     print(request)
+    userw = request.args["username"]
+    passw = request.args["password"]
+    session["username"] = request.args["username"]
+
     if (request.args["username"] == username) & (request.args["password"] == password):
-        return render_template('welcome.html', username = request.args["username"])
+        return render_template('welcome.html', username = request.args["username"], password = request.args["password"])
     else:
-        return render_template('noname.html')
+        return redirect(url_for('after'))
+
 
 if __name__ == "__main__":
     app.debug = True
