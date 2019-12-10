25164148101ab4113203c6599e5c2471b4a13bb1
Alex  Olteanu <alexolteanu@Alexs-MacBook-Pro-2.local>
2019-12-10 10:41:45 -0500

    Finished 27_js

--- 27_js/funky_town.js
+++ 27_js/funky_town.js
@@ -8,6 +8,7 @@ var fact = function(n) {
   }
 }
 
+
 //fibonocci function
 var fib = function(n) {
   if (n == 0) {
@@ -19,4 +20,31 @@ var fib = function(n) {
   else {
     return 1;
   }
+}
+
+
+// gcd(a,b) returns the greatest common divisor of the input numbers a and b
+var gcd = function(a,b) {
+  if (b > a) {var temp=a; a=b; b=temp;}
+  var x = 1;
+  var ans = 0;
+  while (x <= a) {
+    if ((a % x == 0) && (b % x == 0)) {
+      ans = x;
+    }
+    x = x + 1;
+  }
+  return ans;
+}
+
+
+// array of students in SoftDev period 2
+var studentsInPeriod2 = ["abedinM", "avrahamiB", "bhuiyanS", "caoW", "chanM", "chenE", "chenJ", "friedH",
+"huangP", "huangT", "linD", "linW", "lupeaD", "mosesB", "niA", "olinJ", "olteanuA",
+"rahmanM", "sontagC", "thompsonA", "wangD", "wuJ", "yusufovJ", "zenH"];
+
+// randomStudent() returns a random student from the array studentsInPeriod2
+var randomStudent = function() {
+  randomNumber = Math.floor(Math.random() * studentsInPeriod2.length)
+  return studentsInPeriod2[randomNumber]
 }
